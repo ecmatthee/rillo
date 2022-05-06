650f8cd8cb7f75b3e466b3f469b7c4e5de69fe37
Ebert Matthee <ebert@ecmatthee.com>
2022-05-06 09:36:30 +0200

    Changed return type of get_string

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::io;
 
-pub fn get_string() -> io::Result<()> {
+pub fn get_string() -> io::Result<String> {
     let mut buffer = String::new();
     io::stdin().read_line(&mut buffer)?;
-    Ok(())
+    Ok(buffer)
 }
