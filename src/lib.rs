291d863fced7ac043d0d9af1cdfed6fc105a2422
Ebert Matthee <ebert@ecmatthee.com>
2022-05-17 02:14:44 +0200

    Added get_int and get_char functions

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,37 @@ use std::io;
 pub fn get_string() -> io::Result<String> {
     let mut buffer = String::new();
     io::stdin().read_line(&mut buffer)?;
+    trim_newline(&mut buffer);
     Ok(buffer)
+}
+
+// TODO fix handling of "/" and "\".
+// SUGGESTION perhaps check if "/" or "\" after trunc
+pub fn get_char() -> io::Result<String> {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer)?;
+    buffer.truncate(1);
+    Ok(buffer)
+}
+
+pub fn get_int() -> io::Result<i32> {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer)?;
+    let trimmed = buffer.trim();
+    match buffer.trim().parse::<i32>() {
+        Ok(i) => return Ok(i),
+        Err(..) => println!("this was not an integer: {}", trimmed),
+    };
+    // TODO fix error handling (dont return zero on fail)
+    // TODO string.trim() vs fn trim newline
+    Ok(0)
+}
+
+pub fn trim_newline(s: &mut String) {
+    if s.ends_with('\n') {
+        s.pop();
+        if s.ends_with('\r') {
+            s.pop();
+        }
+    }
 }
