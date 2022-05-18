51da6c36b69d1c47452fea2c262daee608239342
Ebert Matthee <ebert@ecmatthee.com>
2022-05-18 14:54:32 +0200

    Complete rewrite of input get_string and get_char, removed get_int

--- src/lib.rs
+++ src/lib.rs
@@ -6,42 +6,34 @@
  * Copyright (c) 2022 Ebert Charles Matthee. All rights reserved.
  */
 
+use crossterm::event::{self, Event, KeyCode, KeyEvent};
 use std::io;
 
-pub fn get_string() -> io::Result<String> {
-    let mut buffer = String::new();
-    io::stdin().read_line(&mut buffer)?;
-    trim_newline(&mut buffer);
-    Ok(buffer)
+pub fn read_line() -> io::Result<String> {
+    let mut line = String::new();
+    while let Event::Key(KeyEvent { code, .. }) = event::read()? {
+        match code {
+            KeyCode::Enter => {
+                break;
+            }
+            KeyCode::Char(c) => {
+                line.push(c);
+            }
+            _ => {}
+        }
+    }
+
+    Ok(line)
 }
 
-// TODO fix handling of "/" and "\".
-// SUGGESTION perhaps check if "/" or "\" after trunc
-pub fn get_char() -> io::Result<String> {
-    let mut buffer = String::new();
-    io::stdin().read_line(&mut buffer)?;
-    buffer.truncate(1);
-    Ok(buffer)
-}
-
-pub fn get_int() -> io::Result<i32> {
-    let mut buffer = String::new();
-    io::stdin().read_line(&mut buffer)?;
-    let trimmed = buffer.trim();
-    match buffer.trim().parse::<i32>() {
-        Ok(i) => return Ok(i),
-        Err(..) => println!("this was not an integer: {}", trimmed),
-    };
-    // TODO fix error handling (dont return zero on fail)
-    // TODO string.trim() vs fn trim newline
-    Ok(0)
-}
-
-pub fn trim_newline(s: &mut String) {
-    if s.ends_with('\n') {
-        s.pop();
-        if s.ends_with('\r') {
-            s.pop();
+pub fn read_char() -> io::Result<char> {
+    loop {
+        if let Event::Key(KeyEvent {
+            code: KeyCode::Char(c),
+            ..
+        }) = event::read()?
+        {
+            return Ok(c);
         }
     }
 }
